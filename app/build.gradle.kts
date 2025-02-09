a31d96663e910104cd720290c5681ffe2ad2c12d
Maycon Henrique <maycon255@hotmail.com>
2025-02-09 15:16:54 +0000

    Setup koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
     buildTypes {
         debug {
-            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/api/v2/\"")
+            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/v2/\"")
         }
 
         release {
@@ -34,7 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/api/v2/\"")
+            buildConfigField("String", "BASE_URL", "\"https://api.coincap.io/v2/\"")
         }
     }
     compileOptions {
---
+++ app/src/main/java/com/plcoding/cryptotracker/CryptoTrackerApp.kt
@@ -0,0 +1,23 @@
+package com.plcoding.cryptotracker
+
+import android.app.Application
+import com.plcoding.cryptotracker.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+/**
+ * Created by Maycon Henrique on 09/02/2025.
+ * maycon255@hotmail.com
+ */
+class CryptoTrackerApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@CryptoTrackerApp)
+            androidLogger()
+
+            modules(appModule)
+        }
+    }
+}
